5b943b961f94028f4dd9b0af8a52ce0bcce61cbb
Craig Gent <genty83@outlook.com>
2024-04-25 14:20:49 +0100

    Added function to switch containers

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,8 +5,24 @@
  */
 
 // Imports - Add all imported functions and classes underneather this line.
+import { SETTINGS, GAME_CONTAINER, HERO_CONTAINER } from "./game.js";
+
+// Constants
+const START_BTN = document.getElementById('start-game-btn');
 
 // *** CODE IN THIS BLOCK WILL ONLY RUN IF THE CONTAINER DISPLAY IS NOT NONE ***
 if (HERO_CONTAINER.style.display != 'none') {
 
+    // Event listeners
+    START_BTN.addEventListener('click', () => {
+        startGame();
+    });
+}
+
+/**Function to log all game settings and switch to the game container */
+function startGame() {
+
+    // Switch container
+    HERO_CONTAINER.style.display = 'none';
+    GAME_CONTAINER.style.display = 'flex';
 }
