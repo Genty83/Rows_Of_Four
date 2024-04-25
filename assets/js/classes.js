cb8a36120c4825f2f32d92e4d332ddcefa4a9929
Craig Gent <genty83@outlook.com>
2024-04-25 20:19:06 +0100

    Added a class to convert passed in color

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -4,6 +4,7 @@
  * Below is a list of the classes present in the file.
  * 
  * >>> GridCell:  This class is used to draw the coin on the board
+ * >>> ConvertColor: This class coverts the passed in color to a darker shade at the % specified
  */
 
 /**Class [GridCell] - This class is used to draw the coin on the board */
@@ -91,5 +92,95 @@ export class GridCell {
       ctx.arc(this.cx, this.cy, this.r, 0, Math.PI * 2);
       ctx.stroke();
     }
+  }
+}
+
+// Class [ConvertColor] - For converting a color passed in lighter or darker
+export class ConvertColor {
+  /**
+   * Class [ConvertColor]: Provides some handy methods to lighten or darken a color passed to the class
+   * @param {string} color - The color to be converted. Can be RGB, hex or hsl
+   * @param {number} percent - The percentage you would like to lighten or darken the color by
+   */
+  constructor(color, percent) {
+    this.color = color;
+    this.percent = percent;
+
+    if (color.charAt(0) == 'h') {
+      this.color = this.hslToHex(this.color);
+    }
+    if (color.charAt(0) == 'r') {
+      this.color = this.RGBToHex(this.color);
+    }
+  }
+
+  /**Method [shadeColor]: shades the color passed by the percentage passed into the constructor */
+  shadeColor() {
+
+    var R = parseInt(this.color.substring(1, 3), 16);
+    var G = parseInt(this.color.substring(3, 5), 16);
+    var B = parseInt(this.color.substring(5, 7), 16);
+
+    R = parseInt(R * (100 + this.percent) / 100);
+    G = parseInt(G * (100 + this.percent) / 100);
+    B = parseInt(B * (100 + this.percent) / 100);
+
+    R = (R < 255) ? R : 255;
+    G = (G < 255) ? G : 255;
+    B = (B < 255) ? B : 255;
+
+    R = Math.round(R);
+    G = Math.round(G);
+    B = Math.round(B);
+
+    var RR = ((R.toString(16).length == 1) ? "0" + R.toString(16) : R.toString(16));
+    var GG = ((G.toString(16).length == 1) ? "0" + G.toString(16) : G.toString(16));
+    var BB = ((B.toString(16).length == 1) ? "0" + B.toString(16) : B.toString(16));
+
+    return "#" + RR + GG + BB;
+  }
+
+  /**Method [RGBToHex]: Can be used to convert an rgb color into a hex color */
+  RGBToHex(rgb) {
+    // Choose correct separator
+    let sep = rgb.indexOf(",") > -1 ? "," : " ";
+    // Turn "rgb(r,g,b)" into [r,g,b]
+    rgb = rgb.substr(4).split(")")[0].split(sep);
+
+    let r = (+rgb[0]).toString(16),
+      g = (+rgb[1]).toString(16),
+      b = (+rgb[2]).toString(16);
+
+    if (r.length == 1)
+      r = "0" + r;
+    if (g.length == 1)
+      g = "0" + g;
+    if (b.length == 1)
+      b = "0" + b;
+
+    return "#" + r + g + b;
+  }
+
+  /**
+   * Method [hslToHex]: Converts a hsl color to an rgb color
+   * @param {string} hsl 
+   */
+  hslToHex(hsl) {
+
+    let sep = hsl.indexOf(",") > -1 ? "," : " ";
+    hsl = hsl.substring(4).split(")")[0].split(sep);
+
+    let h = hsl[0],
+      s = hsl[1].slice(0, -1),
+      l = hsl[2].slice(0, -1);
+
+    l /= 100;
+    const a = s * Math.min(l, 1 - l) / 100;
+    const f = n => {
+      const k = (n + h / 30) % 12;
+      const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+      return Math.round(255 * color).toString(16).padStart(2, '0');
+    };
+    return `#${f(0)}${f(8)}${f(4)}`;
   }
 }
