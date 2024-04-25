80239f22e22bf9d28087b67be7b2633191694476
Craig Gent <genty83@outlook.com>
2024-04-25 14:12:13 +0100

    File setup and initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+/**
+ * Main module:
+ * This is the main js file.
+ * Contains the main block to be called when the site loads
+ */
+
+// Imports - Add all imported functions and classes underneather this line.
+
+// *** CODE IN THIS BLOCK WILL ONLY RUN IF THE CONTAINER DISPLAY IS NOT NONE ***
+if (HERO_CONTAINER.style.display != 'none') {
+
+}
