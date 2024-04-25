fa4f60b4300f94cdd0b19f461f08140c56a4cd4f
Craig Gent <genty83@outlook.com>
2024-04-25 20:19:51 +0100

    Added additional code to startGame function

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,7 @@
 
 // Imports - Add all imported functions and classes underneather this line.
 import { SETTINGS, GAME_CONTAINER, HERO_CONTAINER, gameLoop, newGame } from "./game.js";
+import { ConvertColor } from "./classes.js";
 
 // Constants
 const START_BTN = document.getElementById('start-game-btn');
@@ -43,6 +44,48 @@ OPTIONS_MENU_SAVE_BTN.addEventListener('click', () => {
 
 /**Function to log all game settings and switch to the game container */
 function startGame() {
+
+  // Variables
+  let pName = document.getElementById('p-name').value;
+  let pColor = document.getElementById('p-color').value;
+  let cColor = document.getElementById('c-color').value;
+  let bgColor = document.getElementById('bg-color').value;
+  let brdColor = document.getElementById('brd-color').value;
+  let alertmsg = 'Cannot have 2 colours the same! Please select a different colour!';
+
+  // Conditions
+  if (SETTINGS.playerName != '') {
+    SETTINGS.playerName = pName;
+  }
+
+  if (pColor == cColor || pColor == bgColor || pColor == brdColor) {
+    alert(alertmsg);
+    return;
+  } else {
+    SETTINGS.cColor = cColor;
+    SETTINGS.cColorDark = new ConvertColor(cColor, -40).shadeColor();
+  }
+
+  if (cColor == cColor || pColor == bgColor || cColor == brdColor) {
+    alert(alertmsg);
+    return;
+  } else {
+    SETTINGS.pColor = pColor;
+  }
+  
+  if (bgColor == pColor || bgColor == c || cColor == brdColor) {
+    alert(alertmsg);
+    return;
+  } else {
+    SETTINGS.bgColor = bgColor;
+  }
+
+  if (brdColor == cColor || brdColor == pColor || brdColor == bgColor) {
+    alert(alertmsg);
+    return;
+  } else {
+    SETTINGS.frameColor = brdColor;
+  }
 
   // Switch container
   HERO_CONTAINER.style.display = 'none';
