27657f780d7e62dc0a7e0d12da6e84d28a2c6cbe
Craig Gent <genty83@outlook.com>
2024-05-12 15:10:40 +0100

    Made change to test

--- assets/testing/game.test.js
+++ assets/testing/game.test.js
@@ -2,13 +2,12 @@
  * @jest-environment jsdom
  */
 
+
 import { winner } from "../js/game.js"
 
-
 describe('Testing the winner function', () => {
-
-	test('Check to see if empty array returns false', () => {
-		const arr = [];
-		expect(winner(arr)).toBe(false);
-	})
-}) 
+    test('Check if false is returned', () => {
+        const arr = [];
+        expect(winner(arr)).toBe(false);
+    })
+})
